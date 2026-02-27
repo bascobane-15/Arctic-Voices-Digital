29a9099849a071ecb58c2a6a49c0d7be6c6d2c9f
bascobane-15 <bascobane@gmail.com>
2026-02-27 21:52:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -127,6 +127,21 @@ menu = st.sidebar.selectbox(
         "🎮 Görev Merkezi"
     ]
 )
+with st.sidebar:
+
+    with open("Kuzey Işıkları.mp4", "rb") as f:
+        video_bytes = f.read()
+        video_base64 = base64.b64encode(video_bytes).decode()
+
+    st.markdown(f"""
+<video width="100%" autoplay loop muted playsinline>
+    <source src="data:video/mp4;base64,{video_base64}" type="video/mp4">
+</video>
+""", unsafe_allow_html=True)
+
+
+    st.title("Dijital Arktik Kültür Eğitim Platformu ")
+
 # -------------------------
 # ANA SAYFA
 # -------------------------
