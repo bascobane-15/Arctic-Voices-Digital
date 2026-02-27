80c99f1941951df4202bb58dcfb9e026bd2bcaa9
bascobane-15 <bascobane@gmail.com>
2026-02-27 21:03:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -127,7 +127,29 @@ menu = st.sidebar.selectbox(
         "🎮 Görev Merkezi"
     ]
 )
+with st.sidebar:
 
+    with open("Kuzey Işıkları.mp4", "rb") as f:
+        video_bytes = f.read()
+        video_base64 = base64.b64encode(video_bytes).decode()
+
+    st.markdown(f"""
+<video width="100%" autoplay loop muted playsinline>
+    <source src="data:video/mp4;base64,{video_base64}" type="video/mp4">
+</video>
+""", unsafe_allow_html=True)
+
+
+    st.title("Dijital Arktik Kültür Eğitim Platformu")
+
+    st.markdown("---")
+    st.info("""
+    **Proje Vizyonu:**
+    Bu platform, genç nesillerde insan-merkezli Arktik bilinci oluşturmayı hedefleyen dijital bir eğitim platformudur. 
+    
+# -------------------------
+# ANA SAYFA
+# -------------------------
 if menu == "🏔️ Ana Sayfa":
     
     # 1. HERO BÖLÜMÜ
