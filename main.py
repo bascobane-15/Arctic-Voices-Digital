fb6f4e7fe119708c051e8dd7fe4b10de27c9e07a
bascobane-15 <bascobane@gmail.com>
2026-03-06 00:13:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -420,7 +420,11 @@ elif menu == "🛰️ NASA İklim Verisi":
             title=f"NASA GISTEMP Küresel Sıcaklık Değişimi (Son Ölçüm: {latest_year})"
         )
 
-        
+        st.plotly_chart(fig, use_container_width=True)
+
+        # Durum Renkleri
+        status_color = "rgba(231, 76, 60, 0.2)" if latest_temp > 1.0 else "rgba(52, 152, 219, 0.2)"
+        border_color = "#e74c3c" if latest_temp > 1.0 else "#3498db"
         
         # --- 1. ESTETİK İSTATİSTİK KARTLARI (Cam Efekti) ---
         st.markdown(f"""
