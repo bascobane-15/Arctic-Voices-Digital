56fc486fd0514bcb33c514ca03aa0d7f90dfc32f
bascobane-15 <bascobane@gmail.com>
2026-03-09 23:04:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -863,7 +863,18 @@ elif menu == "🧭 Kültür Keşfi":
 
    
         st.divider()
+        ----
+        st.subheader("Kutup İkliminde Kültür ve Mücadele: Nenetler")
 
+        st.write("""
+        Rusya’nın kuzeyindeki Yamal Yarımadası’nda yaşayan Nenetler: Göçebe ren geyiği çobanlarının zorlu doğada kurduğu geleneksel yaşam ve kültür.
+        """)
+        col1, col2, col3 = st.columns([1,2,1])
+
+        with col2:
+            st.video("https://youtu.be/fVgL4O2d_cQ?si=MJ8lh-KkXkSwcAoN")
+
+        st.divider()
         # --- NENETS DİLİ VE DOĞA  ---
         st.markdown("""
             <div style="background-color: rgba(52, 152, 219, 0.2); 
