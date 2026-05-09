ff9b68f27df484ae2871e4a3b57e703169f3126f
bascobane-15 <bascobane@gmail.com>
2026-05-09 20:35:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ with st.sidebar:
     
     menu = st.selectbox(
         "📍 Keşif Rotası Seçin",
-        ["🏔️ Ana Sayfa", "🗺️ Kültürel Harita", "🛰️ NASA İklim Verisi", "🧭 Kültür Keşfi", " ☾☆ Türkiye'nin Rotası", "🎮 Görev Merkezi"]
+         ["🏔️ Ana Sayfa", "🗺️ Kültürel Harita", "🛰️ NASA İklim Verisi", "🧭 Kültür Keşfi", "🇹🇷 Türkiye'nin Çalışmaları", "🎮 Görev Merkezi"]
     )
 
     st.markdown("---")
@@ -1060,8 +1060,7 @@ elif menu == "🧭 Kültür Keşfi":
 # -------------------------
 # TÜRKİYE'NİN ÇALIŞMALARI
 # -------------------------
-elif menu == "☾☆ Türkiye'nin Rotası":
-
+elif menu == "🇹🇷 Türkiye'nin Çalışmaları":
     # --- SAYFA DÜZENİ VE BOŞLUK AYARLARI (CSS) ---
     st.markdown("""
         <style>
